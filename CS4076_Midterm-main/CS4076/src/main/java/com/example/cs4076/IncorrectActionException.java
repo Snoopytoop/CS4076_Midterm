79146278740e6fb443fae2c1254e146f95aa0d8e
Iwan-Teague <teague.iwan@outlook.com>
2025-03-09 00:41:13 +0000

    Delete CS4076_Midterm-main/CS4076 directory

--- CS4076_Midterm-main/CS4076/src/main/java/com/example/cs4076/IncorrectActionException.java
+++
@@ -1,7 +0,0 @@
-package org.example.midtermproject;
-
-public class IncorrectActionException extends RuntimeException {
-    public IncorrectActionException(String message) {
-        super(message);
-    }
-}
