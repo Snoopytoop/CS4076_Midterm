830be487af771c61865304535cf186c7bb62d715
Iwan-Teague <teague.iwan@outlook.com>
2025-03-09 15:31:31 +0000

    Add files via upload

---
+++ CS4076_Midterm-main/CS4076/src/main/java/com/example/cs4076/Lecture.java
@@ -0,0 +1,20 @@
+package com.example.cs4076;
+
+public class Lecture {
+    private final String name;
+    private final String room;
+
+    public Lecture(String name, String room) {
+        this.name = name;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+}
+
